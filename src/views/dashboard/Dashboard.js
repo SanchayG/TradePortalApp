4198aaf40bbd6901b0e2418f35dc489848cbca4b
SanchayG <78357258+SanchayG@users.noreply.github.com>
2021-02-02 14:52:25 +0530

    Update Dashboard.js

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,10 +1,48 @@
+import React from 'react'
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow
+} from '@coreui/react'
+import { DocsLink } from 'src/reusable'
+
+import usersData from '../users/inboxData'
+const fields = ['Txn No','Product', 'Sub-Product', 'Operation', 'Cust ID', 'Account no', 'Currency', 'Amount', 'Beneficiary Name', 'Bill Ref No', 'Customer Portal Ref No', 'Entry Time']
+
 const Dashboard = () => {
   return (
-    <>
-      
+ 
+<CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Inbox
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable
+              items={usersData}
+              fields={fields}
+              hover
+              striped
+              bordered
+              tableFilter
+              sorter
+              size="sm"
+              itemsPerPage={7}
+              pagination
+              
+            />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
 
-      
-    </>
+
+
   )
 }
 
