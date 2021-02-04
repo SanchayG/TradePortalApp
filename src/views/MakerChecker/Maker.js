9ac1ddf3e019fb424e43575b6c3f964e17f6cb43
AshwiniKurne <61222743+AshwiniKurne@users.noreply.github.com>
2021-02-04 11:02:58 +0530

    aaa

--- src/views/MakerChecker/Maker.js
+++ src/views/MakerChecker/Maker.js
@@ -24,7 +24,7 @@ const Collapses = () => {
   const [accordion, setAccordion] = useState(0)
   const [fade, setFade] = useState(true)
 
-  const toggle = (e) => {
+  const toggle = (e) => { 
     setCollapse(!collapse)
     e.preventDefault()
   }
