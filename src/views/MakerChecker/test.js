79f9a047431640350efe299c8722ed6b4bc325a7
AshwiniKurne <61222743+AshwiniKurne@users.noreply.github.com>
2021-02-10 15:17:28 +0530

    AK_10022020

--- src/views/MakerChecker/test.js
+++ src/views/MakerChecker/test.js
@@ -1,93 +1,20 @@
-import axios from 'axios';
-import React,{Component} from 'react'; 
+import React from "react";
 
-class App extends Component { 
+import { Typography } from "@material-ui/core";
 
-    state = { 
-  
-      // Initially, no file is selected 
-      selectedFile: null
-    }; 
-     
-    // On file select (from the pop up) 
-    onFileChange = event => { 
-      // Update the state 
-      this.setState({ selectedFile: event.target.files[0] }); 
-    }; 
-     
-    // On file upload (click the upload button) 
-    onFileUpload = () => { 
-      // Create an object of formData 
-      const formData = new FormData(); 
-     
-      // Update the formData object 
-      formData.append( 
-        'file', this.state.selectedFile); 
-     
+import UploadFiles from "./UploadFileComponent";
 
-        axios.post("http://localhost:3002/upload", formData, { // receive two parameter endpoint url ,form data 
-      })
-      .then(res => { // then print response status
-        console.log(res.statusText)
-      })  
-      // Details of the uploaded file 
-    // let reader =new FileReader();
-    // reader.readAsDataURL(this.state.selectedFile);
-    // reader.onload=(e)=>{
-    //   console.warn("data ",e.target.result)
-    // }
-      
-	  
-      // Request made to the backend api 
-      // Send formData object 
-     // axios.post("api/uploadfile", formData); 
-	}
-     
-    // File content to be displayed after 
-    // file upload is complete 
-    fileData = () => { 
-      if (this.state.selectedFile) { 
-          
-        return ( 
-          <div> 
-            <h2>File Details:</h2> 
-            <p>File Name: {this.state.selectedFile.name}</p> 
-            <p>File Type: {this.state.selectedFile.type}</p> 
-            <p> 
-              Last Modified:{" "} 
-              {this.state.selectedFile.lastModifiedDate.toDateString()} 
-            </p> 
-          </div> 
-        ); 
-      } else { 
-        return ( 
-          <div> 
-            <br /> 
-            <h4>Choose before Pressing the Upload button</h4> 
-          </div> 
-        ); 
-      } 
-    }; 
-     
-    render() { 
-      return ( 
-        <div> 
-            <h1> 
-              GeeksforGeeks 
-            </h1> 
-            <h3> 
-              File Upload using React! 
-            </h3> 
-            <div> 
-                <input type="file" onChange={this.onFileChange} /> 
-                <button  value ='upload' onClick={this.onFileUpload}> 
-                  Upload! 
-                </button> 
-            </div> 
-          {this.fileData()} 
-        </div> 
-      ); 
-    } 
-  } 
-  
-  export default App; 
+function App() {
+  return (
+    <div className="container">
+      <div className="mg20">
+        <Typography variant="h5">bezkoder.com</Typography>
+        <Typography variant="h6">Material UI File Upload</Typography>
+      </div>
+
+      <UploadFiles />
+    </div>
+  );
+}
+
+export default App;
